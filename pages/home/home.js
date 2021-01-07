4a4b99db5a92c26acb63550a579bad34ea7e2160
Wanlin-Lu <luwanlin@126.com>
2021-01-07 22:23:40 +0800

    wx.setStorageSync && wx.getStorageSync && bindinput & bindtap

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,29 +2,23 @@ const app = getApp()
 
 Page({
   data: {
-    name: '阿西达卡',
-    now: app.globalData.now,
-    items: ['事项A','事项B','事项C']
+    items: [],
+    inputValue: ''
+  },
+  inputHandler(event) {
+    this.setData({
+      inputValue: event.detail.value || ''
+    });
   },
   buttonHandler(event) {
-    const that = this;
-    wx.showModal({
-      title: '操作确认',
-      content: '你确认要修改吗？',
-      success(res) {
-        if (res.confirm) {
-          that.setData({
-            name: '阿桑'
-          }, function () {
-            wx.showToast({
-              title: "操作完成",
-              duration: 700
-            })
-          })
-        } else if (res.cancel) {
-          console.log('用户点击取消');
-        }
-      }
-    });
+    const newItem = this.data.inputValue.trim();
+    if (!newItem) return;
+    const itemArr = [...this.data.items, newItem];
+    wx.setStorageSync('items',itemArr);
+    this.setData({ items: itemArr });
+  },
+  onLoad() {
+    const itemArr = wx.getStorageSync('items') || [];
+    this.setData({ items: itemArr });
   }
 });
