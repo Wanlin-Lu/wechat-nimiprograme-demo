84c96c8adc02a990b455a9ae37964d54d0290aec
Wanlin-Lu <luwanlin@126.com>
2021-01-07 21:48:19 +0800

    wx.showModal({})

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,16 +6,24 @@ Page({
     now: app.globalData.now
   },
   buttonHandler(event) {
-    this.setData(
-      {
-        name: '阿桑',
-      },
-      function () {
-        wx.showToast({
-          title: '操作完成',
-          duration: 700,
-        })
+    const that = this;
+    wx.showModal({
+      title: '操作确认',
+      content: '你确认要修改吗？',
+      success(res) {
+        if (res.confirm) {
+          that.setData({
+            name: '阿桑'
+          }, function () {
+            wx.showToast({
+              title: "操作完成",
+              duration: 700
+            })
+          })
+        } else if (res.cancel) {
+          console.log('用户点击取消');
+        }
       }
-    )
+    });
   }
 });
