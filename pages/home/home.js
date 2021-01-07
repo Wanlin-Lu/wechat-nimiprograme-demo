964d880fcfc117fff39c2f5d34d4446c612f8d7a
Wanlin-Lu <luwanlin@126.com>
2021-01-07 22:49:37 +0800

    open-type='getUserInfo' && bindgetuserinfo && wx.getSetting()

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,12 +1,9 @@
 Page({
-  data: { items: [] },
-  onLoad() {
-    const that = this;
-    wx.request({
-      url: 'http://localhost:3000/items',
-      success(res) {
-        that.setData({ items: res.data });
-      }
+  data: { name: '' },
+  buttonHandler(event) {
+    if (!event.detail.userInfo) return;
+    this.setData({
+      name: event.detail.userInfo.nickName
     });
   }
 });
