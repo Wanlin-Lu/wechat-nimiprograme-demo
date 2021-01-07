ab80377c70eb38119b26bc4b138ce44aa611fa13
Wanlin-Lu <luwanlin@126.com>
2021-01-07 22:36:54 +0800

    wx.request()

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,24 +1,12 @@
-const app = getApp()
-
 Page({
-  data: {
-    items: [],
-    inputValue: ''
-  },
-  inputHandler(event) {
-    this.setData({
-      inputValue: event.detail.value || ''
-    });
-  },
-  buttonHandler(event) {
-    const newItem = this.data.inputValue.trim();
-    if (!newItem) return;
-    const itemArr = [...this.data.items, newItem];
-    wx.setStorageSync('items',itemArr);
-    this.setData({ items: itemArr });
-  },
+  data: { items: [] },
   onLoad() {
-    const itemArr = wx.getStorageSync('items') || [];
-    this.setData({ items: itemArr });
+    const that = this;
+    wx.request({
+      url: 'http://localhost:3000/items',
+      success(res) {
+        that.setData({ items: res.data });
+      }
+    });
   }
 });
